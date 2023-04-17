cf08acad0d9a0f1baa824d76441a928154cd788c
Alexey <arkenstone.12@gmail.com>
2023-04-17 12:08:24 +0500

    addition string in main.py

--- main.py
+++ main.py
@@ -1,9 +1,7 @@
 print('new ' + 'text')
 print(2 + 2)
 print(2 + 3)
-<<<<<<< HEAD
 print("hello")
-=======
 print("test new book")
 print("test git")
->>>>>>> 1fc605db90103c21d39e2a1565c387e9d535e777
+print("test git for Linux")
