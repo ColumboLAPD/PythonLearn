c64639579cae312cd276a9af123fa90ac4d7f781
Alexey <arkenstone.12@gmail.com>
2023-05-01 17:33:45 +0500

    test

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
 print('new ' + 'text')
 print(2 + 2)
 print(2 + 3)
-<<<<<<< HEAD
 print("hello")
-=======
 print("test new book")
 print("test git")
->>>>>>> 1fc605db90103c21d39e2a1565c387e9d535e777
